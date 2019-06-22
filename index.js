74522e8a7759b9bb944e6b9a18e0e265c38e5fe0
dylhack <dylhack@gmail.com>
2019-06-22 12:04:21 -0500

    Removed index items

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
+/* eslint-disable global-require */
 module.exports = {
-	Client: require('./src/Client.js'),
-	command: require('./src/commands'),
-	route: {
-		status: require('./src/routes/status'),
-		playlist: require('./src/routes/playlist')
-	}
+  Client: require('./src/Client.js'),
 };
