2e595141a47524eab074dc86f5c35ad30766cd52
dylhack <dylhack@outlook.com>
2019-08-30 01:38:43 -0500

    Updated VLCPlaylist interface

--- src/types/VLCPlaylist.ts
+++ src/types/VLCPlaylist.ts
@@ -1,3 +1,25 @@
 export interface VLCPlaylist {
+    ro: string;
+    type: string;
+    name: string;
+    id: string;
+    children: VLCPlaylistChild[];
+}
 
+export interface VLCPlaylistChild {
+    ro: string;
+    type: string;
+    name: string;
+    id: string;
+    children: ChildChild[];
+}
+
+export interface ChildChild {
+    ro: string;
+    type: string;
+    name: string;
+    id: string;
+    duration: number;
+    uri: string;
+    current: string;
 }
